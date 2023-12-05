0fdfbcb177f14121dd236c59cb9de721b03ff062
Stefano <stefe68@gmail.com>
2023-12-05 15:18:24 +0100

    Add Form omponent

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import TheForm from "./components/TheForm.jsx";
 function App() {
 	return (
 		<>
+			<h1 className="text-center text-3xl font-bold my-6 text-white">
+				Blog Form
+			</h1>
 			<TheForm />
 		</>
 	);
--- src/components/TheForm.jsx
+++ src/components/TheForm.jsx
@@ -1,2 +1,29 @@
 import {useState} from "react";
-import style from "../css/modules/Form.module.css";
+
+function TheForm() {
+	const [title, setTitle] = useState("");
+
+	function handleTitleChange(event) {
+		setTitle(event.target.value);
+	}
+
+	function handleFormSubmit(event) {
+		event.preventDefault();
+		console.log(title);
+	}
+
+	return (
+		<form onSubmit={handleFormSubmit}>
+			<label htmlFor="title">Titolo: </label>
+			<input
+				type="text"
+				name="title"
+				id="title"
+				value={title}
+				onChange={handleTitleChange}
+			/>
+		</form>
+	);
+}
+
+export default TheForm;
